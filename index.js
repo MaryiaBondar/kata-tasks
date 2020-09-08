b588bb511203fe0432467c55c0b4eedc9880b8f4
MaryiaBondar <69987039+MaryiaBondar@users.noreply.github.com>
2020-09-08 14:48:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function greet(name){
+    return /* строка с именем */
+  }
+
+const areaOrPerimeter = function(l , w) {
+    return /* формула */
+};
+
+function dutyFree(normPrice, discount, hol){
+    return Math.floor(/* формула */)
+  }

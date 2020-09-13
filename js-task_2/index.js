4c6e28bcf6c3cb7f18e72ff2f14b7077e96ece8c
Anton <dragus42@gmail.com>
2020-09-13 17:43:43 +0300

    Added paragraph 2

--- js-task_2/index.js
+++ js-task_2/index.js
@@ -4,4 +4,7 @@ let name = prompt('Ваше имя?'); // Пункт 1
     phone = prompt('Контактый телефон');
     email = prompt('Эл.адрес');
     company = prompt('Компания');
-document.write('Меня зовут '+ name +'. Мне '+ age +' лет.<br> Я проживаю в городе '+ city +' и работаю в компании '+ company +'<br>.Мои контактные данные:<br>'+ phone +', '+ email +'');
+document.write('Меня зовут '+ name +'. Мне '+ age +' лет.<br> Я проживаю в городе '+ city +' и работаю в компании '+ company +'<br>.Мои контактные данные:<br>'+ phone +', '+ email +'<br>');
+
+let birth = 2020 - age; // Пункт 2
+document.write(''+ name +' родился в '+ birth +' году.');

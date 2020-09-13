06a5ec26bfa61f74fafe07636d074784263d9dab
Anton <dragus42@gmail.com>
2020-09-13 17:59:30 +0300

    Added paragraph 3

--- js-task_2/index.js
+++ js-task_2/index.js
@@ -8,3 +8,22 @@ document.write('Меня зовут '+ name +'. Мне '+ age +' лет.<br> Я 
 
 let birth = 2020 - age; // Пункт 2
 document.write(''+ name +' родился в '+ birth +' году.');
+
+let a1 = 3; // Пункт 3
+    a2 = 2;
+    a3 = 5;
+    b1 = 2;
+    b2 = 5;
+    b3 = 4;
+    a = a1 + a2 + a3;
+    b = b1 + b2 + b3;
+if (a == b) {
+    console.log('true');
+}
+else if (a > b) {
+    console.log('false');
+}
+else if(a < b) {
+    console.log('false');
+}
+    

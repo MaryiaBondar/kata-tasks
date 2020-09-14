f1e5b70a70f5035556fcfd57f14e30975839ebca
Nadya <nadejda.skillup@gmail.com>
2020-09-14 23:21:59 +0300

    'js2'

--- js-task_2/index.js
+++ js-task_2/index.js
@@ -1,18 +1,18 @@
 //Задача 1
-//  let name=prompt('Ваше имя?','');
-//  let age=prompt('Ваш возраст?','');
-//  let city= prompt('Ваш город проживания?','');
-//  let phone=prompt('Ваш номер телефона?','');
-//  let email=prompt('Ваш email?','');
-//  let company=prompt('В какой компании Вы работаете?','');
-//  let txt='Меня зовут '+ name+ '. Мне '+age+` лет.
-//  Я проживаю в городе `+city+' и работаю в компании '+company+'.Мои контактные данные:'+phone+','+email+'.';
-//alert(txt);
+ let name=prompt('Ваше имя?','');
+ let age=prompt('Ваш возраст?','');
+ let city= prompt('Ваш город проживания?','');
+ let phone=prompt('Ваш номер телефона?','');
+ let email=prompt('Ваш email?','');
+ let company=prompt('В какой компании Вы работаете?','');
+ let txt='Меня зовут '+ name+ '. Мне '+age+` лет.
+ Я проживаю в городе `+city+' и работаю в компании '+company+'.Мои контактные данные:'+phone+','+email+'.';
+alert(txt);
 
 // Задача 2
-// let date=new Date();
-// let year=date.getUTCFullYear()
-// alert(name+' родился в '+ (year-age)+' году.');
+let date=new Date();
+let year=date.getUTCFullYear()
+alert(name+' родился в '+ (year-age)+' году.');
 
 //Задача 3
 let line='123322';
@@ -25,7 +25,7 @@ let a=-3;
 (a>0) ? console.log('Верно'):console.log('Неверно')
 
 // Задача 5
-a=11;
+a=10;
 let b=2;
 console.log('a='+a+ ', b='+b);
 console.log("Сумма a и в = "+ (a+b));
@@ -37,7 +37,3 @@ console.log("Частное a и в = "+ (a/b));
 //Задача 6
  ( ((a>2) && (a<11)) || ((b>=6) && (b<14)) ) ? console.log('Верно'):console.log('Неверно');
 
-
-
-// Если переменная a (из задания 5) больше 2 и меньше 11, или переменная b (из задания 5) больше или равна 6 и меньше 14, то выведите 'Верно', в противном случае выведите
-// 'Неверно'.

ee1b934538ff66e683ce50512b597fec73f1e94b
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2020-09-15 15:18:26 +0300

    Update index.js

---
+++ js-task_1/index.js
@@ -0,0 +1,60 @@
+let a1 = 3 % 5;
+    a2 = 5 + 3;
+    a3 = 5 - 3;
+    a4 = 75 + 'кг';
+    a5 = 785 * 653;
+    a6 = 100 / 25;
+    a7 = 0 * 0;
+    a8 = 0 / 2;
+    a9 = 89 / 0;
+    a10 = 98 + 2;
+    a11 = 5 - 98;
+    a12 = (8 + 56 * 4) / 5;
+    a13 = (9 - 12) * 7 / (5 + 2);
+    a14 = + "123";
+    a15 = true > 0;
+    a16 = 1 || 0;
+    a17 = false || true;
+    
+
+
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a17, typeof a16);
+console.log(a16, typeof a17);
+
+
+  VCilindra = 3.14 * 10 * a7 * a7 /4;
+  console.log(VCilindra);
+  SKruga = 3.14 * 5 * 5;
+  console.log(SKruga);
+  STrap = 0.5 * 10 * (5 + 7);
+  console.log(STrap);
+  ur1 = (-45 - 8) / (3+6)
+console.log(ur1);
+ur2 = 23780 / (2 + 8 + 3);
+console.log(ur2);
+
+let text = 'индо земля зашаталась под ногами-и вырос',
+    text2 = 'и заревел он голосом диким...',
+    text3 = 'блеснула молния и ударил гром,',
+    text4 = 'а так какое-то чудище, страшное и мохнатое,',
+    text5 = 'как будто из-под земли, перед купцом:',
+    text6 = 'Он подошёл и сорвал аленький цветочек.',
+    text7 = 'зверь не зверь, человек не человек,',
+    text8 = 'В ту же минуту, безо всяких туч,',
+    console.log(text6 + text8 + text3 + text + text5 + text7 + text4 + text2);
+  

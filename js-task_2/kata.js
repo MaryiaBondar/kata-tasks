82362f84d2188a74abc240ce4b6af7ba05d5558d
Dmitri <d.sadoshenko@gmail.com>
2020-09-15 22:24:28 +0300

    second commit

--- js-task_2/kata.js
+++ js-task_2/kata.js
@@ -1,12 +1,28 @@
 function basicOp(operation, value1, value2){
-   return //результат операции
+    if(operation == '+')return value1 + value2;
+    if(operation == '-')return value1 - value2;
+    if(operation == '*')return value1 * value2;
+    if(operation == '/')return value1 / value2;
 }
 
 function bonusTime(salary, bonus) {
-    return //зарплата с бонусом или без
+    if (bonus === true) {
+      return '£' + (salary * 10)
+    } else {return '£' + salary
+    }
 }
 
 function getGrade (s1, s2, s3) {
-    let avg = (s1+s2+s3)/3;
-    return //буква
+    let avg = (s1 + s2 + s3) / 3;
+    if (avg >= 90 && avg <= 100) {
+      return 'A';
+  } else if (avg >= 80 && avg < 90) {
+    return 'B';
+  } else if (avg >= 70 && avg < 80) {
+    return 'C';
+  } else if (avg >= 60  && avg < 70) {
+    return 'D';
+  } else {
+    return 'F';
+  }
 }
---
+++ js-task_2/task3.js
@@ -0,0 +1,14 @@
+// Задание 3
+let a = 1;
+    b = 2;
+    c = 3;
+    d = 4.5;
+    f = 0.5;
+    g = 6;
+    x = a + b + c;
+    y = d + f + g;
+if (x == y) {
+    console.log('да');
+} else {console.log('нет');
+} 
+

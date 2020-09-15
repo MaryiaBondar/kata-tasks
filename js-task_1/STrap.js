64400a04681384f5b392ffea3aa5ea7a8485a75f
Dmitri <d.sadoshenko@gmail.com>
2020-09-15 22:21:41 +0300

    first commit

---
+++ js-task_1/STrap.js
@@ -0,0 +1,6 @@
+// Задание 5
+let a = 5;
+    b = 7;
+    STrap = (a + b) / 2 * h;
+
+    console.log(STrap);
---
+++ js-task_1/task6.js
@@ -0,0 +1,10 @@
+// Задание 6
+let A = 8;
+    B = 3;
+    x1 = (16 - A) / 2 + B;
+    x2 = (A - 15 * B) / (B - 6);
+    x3 = 23780 / (3 + A + B);
+
+console.log(x1);
+console.log(x2);
+console.log(x3);

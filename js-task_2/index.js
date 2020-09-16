f346d5c409356bb92c3bc3c492bf9126285bdb81
Alexandr <frezar2007@gmail.com>
2020-09-16 03:20:31 +0300

    js-task2-30%

--- js-task_2/index.js
+++ js-task_2/index.js
@@ -1,1 +1,45 @@
+//task_1
+/*
+let name = prompt("Ваше имя"),
+    age = prompt("Ваш возраст"),
+    city = prompt("Ваш город"),
+    phone = prompt("Ваше телефон"),
+    email = prompt("Ваше емейл"),
+    company = prompt("Ваше компания");
+
+document.write(`<h1><font color="red">Task_1</font></h1><h4>Меня зовут ${name}.<br> Мне ${age} лет.<br>
+    Я проживаю в городе ${city} и работаю в компании ${company}.<br>
+    Мои контактные данные:<br>
+    Телефон: ${phone} <br> Email: ${email}</h4><br><hr>`);
+
+//task_2
+
+let YearOfBirth = 2020 - age;
+
+document.write(`<h1><font color="red">Task_2</font></h1><h2>${name} родился в ${YearOfBirth} году.</h2><br><hr>`);
+
+//task_3
+
+document.write(`<h1><font color="red">Task_3</font></h1>`)
+let a1, b1, c1, d1, f1, g1;
+if (a1 + b1 + c1 == d1 + f1 + g1) {
+    document.write('<strong>Верно</strong><br><hr>');
+} else {
+    document.write('<strong>Не верно</strong><br><hr>');
+}
+*/
+
+//task_4
+
+let a;
+if (a > 0) {
+    document.write('Верно <hr>')
+} else {
+    document.write('Не верно <hr>')
+}
+//task_5
+document.write(`<h1><font color="red">Task_5</font></h1>`)
+
+
+
 

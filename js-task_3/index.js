81f1db3c3e41e7353d7ab01b084d4786311bff0a
Anton <dragus42@gmail.com>
2020-09-16 13:28:05 +0300

    paragraph 3

--- js-task_3/index.js
+++ js-task_3/index.js
@@ -1,14 +1,24 @@
 // Задание 1
-for(let i = 1; i <= 50; i++) {   
+for (let i = 1; i <= 50; i++) {   
     console.log(i);
 }
 
-for(let i = 35; i >= 8; i--) {
+for (let i = 35; i >= 8; i--) {
     console.log(i);
 }
 // Задание 2
 let e = 89;
-while(e >= 11) {
+while (e >= 11) {
     document.write(e + '<br>');
     e--;
+}
+// Задание 3 
+let a = 100;
+let sum = 0;
+for (let r = 0; r <= a; r++) {
+    sum = 0;
+    for (let j = 1; j <= r; j++) {
+        sum +=j;
+    }
+    console.log('Sum' + r + ' = ' + sum);
 }

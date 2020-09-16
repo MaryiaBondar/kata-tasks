b8addee83a01b29c548c7bf8022ca26b6e372f77
Anton <dragus42@gmail.com>
2020-09-16 15:10:47 +0300

    paragraph 6

--- js-task_3/index.js
+++ js-task_3/index.js
@@ -33,3 +33,32 @@ for (i = 8; i <= 56; i++) {
     if (i % 2 == 0)
     console.log(i);
 }
+// Задание 6
+for ( i=1; i<=10; i++) {
+    document.write ("2*" + i + "=" + 2*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("3*" + i + "=" + 3*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("4*" + i + "=" + 4*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("5*" + i + "=" + 5*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("6*" + i + "=" + 6*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("7*" + i + "=" + 7*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("8*" + i + "=" + 8*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("9*" + i + "=" + 9*i + '<br>');
+}
+for ( i=1; i<=10; i++) {
+    document.write ("10*" + i + "=" + 10*i + '<br>');
+}
+

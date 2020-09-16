545c11761c30326c4efaf75cdf7f45ab8eea52e4
Ivan3568323 <70649262+Ivan3568323@users.noreply.github.com>
2020-09-16 23:24:10 +0300

    второй таск

--- js-task_2/index.js
+++ js-task_2/index.js
@@ -1,12 +1,68 @@
 //1
 
-name, age, city, phone, email
 
-let name = 'Майк';
-    age = 35;
-    city = 'Berlin';
-    phone = '+375336669966';
-    email = 'novinki@gmail.com';
-    company = 'CompanyLTD';
+let name = prompt( 'Ваше имя'),
+    age = prompt( 'Ваш возраст' ), 
+    city = prompt( 'Ваш город проживания' ),
+    phone = prompt('Ваш номер телефона' ),
+    email = prompt( 'Ваш е-мейл' ),
+    company = prompt( 'Название Вашей компании' );
+
+document.write( ' Меня зовут ' + name + '. ' + 'Мне ' + age + ' лет. ' );
+document.write( ' Я проживаю в городе ' + city + ' и работаю в компании ' + company + '. ' );
+document.write( ' Мои контактные данные: ' );
+document.write( phone + ', ' + email + '.');
+
+//2
+
+const year = 2020;
+let born = year - age;
+
+document.write( name + ' родился в ' + born + ' году. ');
 
 
+
+//3
+
+let a = 10,
+    b = 2,
+    c = 3,
+    d = 4,
+    e = 5,
+    f = 6;
+
+let x = a + b + c,
+    y = d + e + f;
+
+if (x === y) {
+    console.log('Да');
+} else {
+    console.log('Неа');
+}
+
+//4
+
+if (a > 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+//5
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+if (a + b > 0) {
+    console.log((a + b) * (a + b));
+}
+
+//6
+
+if (a > 2 && a < 11 || b >= 6 && b < 14) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
--- js-task_2/kata.js
+++ js-task_2/kata.js
@@ -1,12 +1,51 @@
-function basicOp(operation, value1, value2){
-   return //результат операции
+function basicOp(operation, value1, value2)  //1
+{
+  let result
+  switch(operation) {
+      case '+':
+      result = value1 + value2;
+      break;
+      case '-':
+      result = value1 - value2;
+      break;
+      case '*':
+      result = value1 * value2;
+      break;
+      case '/':
+      result = value1 / value2;
+  }
+  
+  return result
 }
 
+
 function bonusTime(salary, bonus) {
-    return //зарплата с бонусом или без
+    return '£${bonus ? salary * 10 : salary}'
 }
 
 function getGrade (s1, s2, s3) {
-    let avg = (s1+s2+s3)/3;
-    return //буква
+    function getGrade (s1, s2, s3) {
+        const avg = (s1 + s2 + s3) / 3
+        let result = 'F'
+        
+        switch(true) {
+            case (avg >= 90) :
+            result = 'A'
+            break;
+            
+            case (avg >= 80) :
+            result = 'B'
+            break;
+            
+            case (avg >= 70) :
+            result = 'C'
+            break;
+            
+            case (avg >= 60) :
+            result = 'D'
+            break;  
+        }
+        
+        return result
+      }
 }

d4aa01ddc0a89002f25b5e8f9ec6656e351c9596
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2020-09-17 18:42:36 +0300

    Update index.js

--- js-task_3/index.js
+++ js-task_3/index.js
@@ -1,1 +1,49 @@
 
+let i = 1;
+while(i <= 50) { console.log(i);
+           i++; }
+
+
+
+   let a = 89;
+  while(a >= 11) { console.log(a);
+                      a--; }
+           
+
+let b = 100;
+let sum = 0;
+for (let c = 1; c <= b; c++) { console.log(c); sum += c; }
+console.log(sum); 
+
+
+let d = 5;
+let sum2 = 0;
+for (let f = 1; f <= d; f++)
+{ sum2 = 0; for (let j = 1; j <= f; j++) { sum2 += j; }
+ console.log('Sum2 ' + f + ' = ' + sum2);  }
+
+ 
+ 
+ for (let h = 8; h <= 56; h++) {
+     if(h % 2 == 1) continue;
+    console.log(h); }
+
+   
+    
+
+    for (let k = 1; k <= 10; k++)
+    {   for (let m = 1; m <= 10; m++) 
+     console.log( k + ' * ' + m + ' = ' + (k*m));  }
+
+
+    
+    
+     for (let n = 1000, num = 0; n > 50; num++) {
+       t = n /= 2; if( t < 50) break;
+        
+        console.log(t); }
+     
+
+
+   
+

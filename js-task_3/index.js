c9d22689f50ee2fb6f4563e16cd44a0204b908f9
Ivan3568323 <70649262+Ivan3568323@users.noreply.github.com>
2020-09-17 18:57:01 +0300

    сделал 1-5 только

---
+++ js-task_3/index.js
@@ -0,0 +1,59 @@
+//1.1 первое условие от 1 до 50
+
+
+let i = 1;
+
+while(i <= 50) {
+    console.log(i);
+    i++;
+}
+
+//1.2 второе условие от 32 до 8ми
+
+for(let y = 32; y >= 8; y--) console.log(y);
+    
+
+//2  Второе задание выполнено в index.html
+
+//3  Пришлось создать отдельный js файл
+
+//4
+
+let a = 5;
+let sum = 0;
+
+for (let e = 1; e <= a; e++) {
+    sum = 0;
+
+    for (let j = 1; j <= e; j++) {
+        sum += j;  
+    }
+    console.log('Sum' + e + '=' + sum);
+}
+
+
+//5
+
+
+for(let u = 0; u <=56; u++) {
+    if (u % 2 !== 0) continue;
+
+    console.log(u);
+}
+
+
+//6  
+
+
+
+
+
+//7
+
+
+
+
+
+
+
+
---
+++ js-task_3/task3.js
@@ -0,0 +1,7 @@
+
+let summ = 0;
+
+for(let i = 1; i <= 100; i++){
+    summ += i;
+}
+ console.log(summ);

17c985f1511ee39d015ab42412301da83b18245c
Anton <dragus42@gmail.com>
2020-09-21 18:47:29 +0300

    paragraph 2

--- js-task_4/index.js
+++ js-task_4/index.js
@@ -2,3 +2,12 @@
 
 let arr = [1, 2, 3, 4, 5]
 for (let i = 0; i < arr.length; i++) console.log(arr[i]);
+
+// Задание 2
+
+let arr1 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7]
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] < 0 && arr1[i] > -10 && arr1[i] < -3) {
+        console.log(arr1[i])
+    }
+}

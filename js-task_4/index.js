ab750fa14c955d7f851247bb40b7071bdeaa5227
Anton <dragus42@gmail.com>
2020-09-21 19:03:50 +0300

    paragraph 3 and 4

--- js-task_4/index.js
+++ js-task_4/index.js
@@ -11,3 +11,33 @@ for (let i = 0; i < arr1.length; i++) {
         console.log(arr1[i])
     }
 }
+
+// Задание 3
+
+let arr2 = [],
+    result = 0;
+for (let i = 23; i < 58; i++) {
+    arr2.push(i);    
+}
+
+let arr3 = [];
+let i = 23;
+while (i < 58) {
+    arr3.push(i);
+    i++;
+}
+for (let i=0 ; i < arr2.length; i++) {
+    result +=arr2[i];
+}
+console.log(arr3)
+console.log(arr2)
+console.log(`sum = `+result)
+
+// Задание 4
+
+let arr4 = [],
+    arr3 = ['10', '20', '30', '50', '235', '3000'];
+for (let i = 0; i <= arr3.length -1; i++) {   
+    arr4.push(+arr3[i])
+}
+console.log(typeof arr4, arr4.filter(num=>['1','2','5'].includes(num.toString()[0])))

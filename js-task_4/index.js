8a4eabd27cea57cc91bedf7372815c76bd1323a4
Ivan3568323 <70649262+Ivan3568323@users.noreply.github.com>
2020-09-22 19:15:00 +0300

    это фиаско

---
+++ js-task_4/index.js
@@ -0,0 +1,73 @@
+
+
+//1 
+
+let nums = [1, 2, 3, 4, 5];
+for(let i = 0; i < nums.length; i++) {
+    document.write(nums[i]);
+}
+
+
+//2  не сделал
+
+let num = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+
+let numNegative = [];
+    for (let i = 0; i < 0; i++); {
+        if (numNegative > -10 && numNegative < -2) {
+            numNegative.push(num[i]);
+        }
+    }
+
+    console.log(numNegative);
+
+
+//3  
+
+let arr =[];
+for(let i = 23; i <=57; i++) {
+    arr.push(i);
+}
+    document.write(arr + ';   ');
+
+let result = 23;
+for(let i = 1; i <= 57; i++){
+        result += i;
+    }
+    document.write('Сумма чисел от 23 до 57 = ' + result);
+
+
+
+//4 не сделан
+
+let arr = ['10', '20', '30', '50', '235', '3000'];
+console.log(arr);
+
+alert(arr.length);
+
+
+//6 
+
+let rainbow = ['каждый ', 'охотник ', 'желает ', 'знать ', 'где ', 'сидит '];
+rainbow[rainbow.length] = 'олень';
+console.log(rainbow);
+
+
+
+//8 
+
+let other = [12, false, 'Текст', 4, 2, -5, 0];
+other.reverse();
+alert(other);
+
+
+
+//9  не готов
+
+let amount = [5, 9, 21, , , 9, 78, , , , 6];
+console.log(amount.length);
+alert('Количество пустых элементов в массиве = ' + '');
+
+
+
+

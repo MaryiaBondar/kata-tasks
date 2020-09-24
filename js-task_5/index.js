76e4a3ae3543923a0d9a33b6a968b0dff2e9425a
Ivan3568323 <70649262+Ivan3568323@users.noreply.github.com>
2020-09-24 19:11:19 +0300

    Add files via upload

---
+++ js-task_5/index.js
@@ -0,0 +1,92 @@
+
+/*
+//1 
+function countMe (a, b, c) {
+    alert((a - b) / c);
+}
+countMe(6, 4, 2);
+*/
+
+
+/*
+
+//2 
+function square(a) {
+    return a * a;
+}
+
+let b = square(3);
+
+console.log(b);          
+
+
+function cube(a) {
+    return a * a * a;
+}
+
+let c = cube(5);
+
+console.log(c); 
+
+*/
+
+/*
+
+//3
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+let c = min(10, 8);
+console.log(c);
+
+function max(d, e) {
+    if (d > e) {
+        return d;
+    } else {
+        return e;
+    }
+}
+
+let f = max(100, 58);
+console.log(f);
+
+*/
+
+
+/*
+//5
+function isEven (i, r) {
+    if (i % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let i = isEven(5);
+console.log(i);
+
+
+//6
+
+function createArray(length) {
+    length = length || 10;
+    let array = [];
+
+    for(let a = 1; a <= length; a++) { 
+        array.push(a);
+    }
+    
+    console.log(array);   //я не понял куда вкрутить isEven
+}
+
+createArray(14);
+
+*/
+

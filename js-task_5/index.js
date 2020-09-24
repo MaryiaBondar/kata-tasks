2f4c3480dcebfc600d6b39078fd89bcca3d6d23d
Anton <dragus42@gmail.com>
2020-09-24 17:15:20 +0300

    paragraph 3

--- js-task_5/index.js
+++ js-task_5/index.js
@@ -8,7 +8,17 @@
 
 // Задание 2
 
-function cub(num) {
-    return num * num * num;
-    }
-console.log(cub(4));
+// function cub(num) {
+//     return num * num * num;
+//     }
+// console.log(cub(4));
+
+
+// Задание 3
+
+let a = prompt('число 1'),
+    b = prompt('число 2');
+    c = Math.max(a, b);
+    d = Math.min(a, b)
+console.log('max' + c);
+console.log('min' + d);

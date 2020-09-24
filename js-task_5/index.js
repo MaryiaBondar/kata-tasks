3d322cf89acf70e1ea43fcbfd69e2c02f23fb188
Anton <dragus42@gmail.com>
2020-09-24 17:11:53 +0300

    paragraph 1 and 2

--- js-task_5/index.js
+++ js-task_5/index.js
@@ -1,1 +1,14 @@
 
+// Задание 1
+
+//  let f = function(a, b, c){
+//      return (a - b) / c;
+//      }
+//  console.log(f(15, 5, 2));
+
+// Задание 2
+
+function cub(num) {
+    return num * num * num;
+    }
+console.log(cub(4));

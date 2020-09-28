187e43a74cbd44fe37007409823f6841f260afe7
Alexandr <frezar2007@gmail.com>
2020-09-28 22:20:17 +0300

    30%

--- js-task_6/index.js
+++ js-task_6/index.js
@@ -1,2 +1,13 @@
-let a='aaa@bbb@ccc';
-console.log(a.replace(/@/gi,'!'))
+// //task_1
+// let a = "aaa@bbb@ccc";
+// console.log(a.replace(/@/gi, "!"));
+
+// //task_2
+// let a2 ='2025-12-31';
+// console.log(a2.replace(/(2025)-(12)-(31)/, '$3/$2/$1'))
+
+//task_3
+let a2='Я учу javascript!'
+console.log(a2.slice(0,1)+a2.slice(16,17)+'---'+'Метод Slice')
+// console.log(a2.)
+// console.log(a2.)

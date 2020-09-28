0f229d56c6bd6e1c09cea7f2b36b62c477da18d1
Alexandr_netbook <frezar2007@gmail.com>
2020-09-28 23:44:10 +0300

    40%

--- js-task_6/index.js
+++ js-task_6/index.js
@@ -6,8 +6,17 @@
 // let a2 ='2025-12-31';
 // console.log(a2.replace(/(2025)-(12)-(31)/, '$3/$2/$1'))
 
-//task_3
-let a2='Я учу javascript!'
-console.log(a2.slice(0,1)+a2.slice(16,17)+'---'+'Метод Slice')
-// console.log(a2.)
-// console.log(a2.)
+// //task_3
+// let a2='Я учу javascript!'
+// console.log(a2.slice(0,1)+a2.slice(16,17)+'---'+'Метод Slice')
+// console.log(a2.substring(0,1)+a2.substring(16,17)+'---'+'Метод substring')
+// console.log('Substr устарел')
+
+// //task_4
+// let a3=[4, 2, 5, 19, 13, 0, 10],
+// sum=0;
+// for (let i = 0; i < a3.length; i++) {
+    
+//     sum+=Math.pow(a3[i],3)
+// }
+// console.log(Math.pow(sum,2))

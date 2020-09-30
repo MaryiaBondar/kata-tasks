ec31c0db3b3fd0eb4ed106e437bdc8b1ef185ece
Anton <dragus42@gmail.com>
2020-09-30 15:01:36 +0300

    task 5

--- js-task_5/index.js
+++ js-task_5/index.js
@@ -24,6 +24,17 @@
 // console.log('max' + c);
 // console.log('min' + d);
 
+// Задание 4
+
+// function createArray (length) {
+//     let array = [];
+//     for (let i = +prompt ('начальное число диапозона'); i <= length; i++ ) {
+//         array.push (i);
+//     }
+//     console.log (array);
+// }
+// createArray(15);
+
 // Задание 5
 // function isEven(num) {
 //     if(num > 0 && num % 2 == 0) {
@@ -31,4 +42,40 @@
 //     }else return false;
 // }    
 // console.log(isEven(12));
+
+// Задание 6
+
+// let arr = [2, 12, 8, 9, 24, 6, 12, 16, 9, 0, 15, 3, 5];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (isEven(arr[i])) {
+//         newArr.push(arr[i]);
+//     }
+// }
+// alert(newArr);
+
+// Задание 7
+
+// function piramid (a, b) {
+//     if (+a == NaN) return;
+//     for (let i = 1; i <= a; i++) {
+//       let c = '';
+//       for (let j = 0; j < i; j++) {
+//         c += b == undefined || b == '' ? i : b;
+//       }
+//       console.log(c + '\a');
+//     }
+//   }
+//   piramid(prompt('Кол-во рядов'), prompt('символ'));
+
+// Задание 8 
+
+// let arr = [7, 10, 4, 7, 3, 8, -2];
+// function func(arr, a) {
+//     if (a < arr.length) {
+//         console.log(arr[a]);
+//         return func(arr, a + 1);
+//     }
+// }
+// func(arr, 0);
 
